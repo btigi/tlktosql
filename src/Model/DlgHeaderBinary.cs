1e0f46546566cac0b7580c3b4200c0c1c863d9ed
Bastien Vandamme <bastien.vandamme@newsprintgroup.com>
2023-06-21 16:29:45 +0800

    Added DlgHeader and DlgState models. I want to be able to read these files.

--- src/Model/DlgHeaderBinary.cs
+++ src/Model/DlgHeaderBinary.cs
@@ -1,14 +1,47 @@
-﻿using System.Runtime.InteropServices;
+﻿/* DLG V1 Header https://gibberlings3.github.io/iesdp/file_formats/ie_formats/dlg_v1.htm#formDLGV1_Header
+ * 
+ * Offset	Size (data type)	Description
+ * 0x0000	4 (char array)	Signature ('DLG ')
+ * 0x0004	4 (char array)	Version ('V1.0')
+ * 0x0008	4 (dword)	Number of states
+ * 0x000c	4 (dword)	Offset of state table from start of file
+ * 0x0010	4 (dword)	Number of transitions
+ * 0x0014	4 (dword)	Offset of transition table from start of file
+ * 0x0018	4 (dword)	Offset of state trigger table from start of file
+ * 0x001c	4 (dword)	Number of state triggers
+ * 0x0020	4 (dword)	Offset of transition trigger table from start of file
+ * 0x0024	4 (dword)	Number of transition triggers
+ * 0x0028	4 (dword)	Offset of action table from start of file
+ * 0x002c	4 (dword)	Number of actions
+ * 0x0030	4 (dword)	Flags specifying what the creature does when the dialog is interrupted by a hostile action from a EA < GOODCUTOFF creature.
+ * * Bit 0: Enemy()
+ * * Bit 1: EscapeArea()
+ * * Bit 2: nothing (but since the action was hostile, it behaves similar to bit 0)
+ * * In addition, all these bits trigger the not-pause behaviour.
+ * * Hostile action is something like an effect from a spell with the hostile flag, damage effect, a simple attack or a failed pickpocket action.
+ * * Note: This field does not exist in DLG files used in BG1.
+*/
+
+using System;
+using System.Runtime.InteropServices;
 
 namespace TlkToSql.Model
 {
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     struct DlgHeaderBinary
     {
-        public Array4 ftype;
-        public Array4 fversion;
-        public short LanguageId;
-        public int StringCount;
-        public int StringOffset;
+        public Array4 signature;
+        public Array4 version;
+        public int Number_Of_state;
+        public int Offset_of_state; // table from start of file;
+        public int Number_of_transitions;
+        public int Offset_of_transition; // table from start of file;
+        public int Offset_of_state_trigger; // table from start of file;
+        public int Number_of_state_triggers;
+        public int Offset_of_transition_trigger; //  table from start of file;
+        public int Number_of_transition_triggers;
+        public int Offset_of_action; // table from start of file;
+        public int Number_of_actions;
+        public int Flags; // Flags specifying what the creature does when the dialog is interrupted by a hostile action from a EA<GOODCUTOFF creature.
     }
 }
--- src/Model/DlgStateBinary.cs
+++ src/Model/DlgStateBinary.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/* DLG V1 State table (actor responses) https://gibberlings3.github.io/iesdp/file_formats/ie_formats/dlg_v1.htm#formDLGV1_State
+ * 
+ * Offset	Size (data type)	Description
+ * 0x0000	4 (strref)	Actor response text (i.e. what the non-player character says to the party)
+ * 0x0004	4 (dword)	Index of the first transition corresponding to this state (i.e. the index in the transition table of the first potential response the party can make in this state).
+ * 0x0008	4 (dword)	Number of transitions corresponding to this state (i.e. how many possible responses are there to this state). A consecutive range of transitions in the transition table are assigned to this state, starting from 'first', as given by the previous field, ranging up to (but not including) 'first'+'count'.
+ * 0x000c	4 (dword)	Trigger for this state (as index into the state trigger table), or 0xFFFFFFFF if no trigger is used for this state.
+ * 
+ * */
+
+using System;
 using System.Runtime.InteropServices;
 
 namespace TlkToSql.Model
@@ -6,11 +16,10 @@ namespace TlkToSql.Model
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     struct DlgStateBinary
     {
-        public short Flags;
-        public Array8 Sound;
-        public Int32 VolumeVariance;
-        public Int32 PitchVariance;
-        public Int32 StringIndex;
-        public Int32 StringLength;
+
+        public Int32 actor;                     // Actor response text (i.e. what the non-player character says to the party)
+        public Int32 name;                      // Index of the first transition corresponding to this state (i.e. the index in the transition table of the first potential response the party can make in this state).
+        public Int32 number_of_transisiton;     // Number of transitions corresponding to this state (i.e. how many possible responses are there to this state). A consecutive range of transitions in the transition table are assigned to this state, starting from 'first', as given by the previous field, ranging up to (but not including) 'first'+'count'.
+        public Int32 trigger;                   // Trigger for this state (as index into the state trigger table), or 0xFFFFFFFF if no trigger is used for this state.
     }
 }
